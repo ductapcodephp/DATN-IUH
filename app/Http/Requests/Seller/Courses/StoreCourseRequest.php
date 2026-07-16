0330e63d9ad5a12ab75bd77d78fc66d8633631e7
duccodephp <ductapcode@gmail.com>
2026-07-16 21:16:17 +0700

    [update]: clean request

--- app/Http/Requests/Seller/Courses/StoreCourseRequest.php
+++ app/Http/Requests/Seller/Courses/StoreCourseRequest.php
@@ -6,19 +6,12 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class StoreCourseRequest extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     */
+    
     public function authorize(): bool
     {
-        return true; // We use controller middleware or custom logic to authorize
+        return true;
     }
 
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
-     */
     public function rules(): array
     {
         return [
